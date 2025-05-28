18061f83b824aa0b543acd430825c405553160b6
mauricioarrequena <mauricio.ar.requena@gmail.com>
2025-05-27 22:14:17 -0400

    fix:corrected some bugs at saving data and visuals

--- src/components/AddHabit.tsx
+++ src/components/AddHabit.tsx
@@ -1,6 +1,6 @@
 import styles from "../styles/components/AddHabit.module.css";
 import type { Habit } from "../Types/Habit";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 interface AddHabitProps {
   mode: string;
@@ -17,6 +17,11 @@ export default function AddHabit({
   const [habitName, setHabitName] = useState<string>("");
   const [selectedColorId, setSelectedColorId] = useState<string>("");
   const [habitOnEdition, setHabitOnEdition] = useState<Habit>(null!);
+  const refs = {
+    name: useRef<HTMLInputElement>(null),
+    color: useRef<HTMLInputElement>(null),
+  };
+
 
   type Color = {
     id: string;
@@ -54,6 +59,21 @@ export default function AddHabit({
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     const foundColor = colors.find((color) => color.id === selectedColorId)!;
+
+    const nameErrorMessage = refs.name.current!.nextElementSibling;
+    if (!habitName) {
+      nameErrorMessage?.classList.add(`${styles.errorMessageShown}`)
+      return;
+    }
+    nameErrorMessage?.classList.remove(`${styles.errorMessageShown}`)
+
+    const colorErrorMessage = refs.color.current!.nextElementSibling;
+    if (!selectedColorId) {
+      colorErrorMessage?.classList.add(`${styles.errorMessageShown}`)
+      return;
+    }
+    colorErrorMessage?.classList.remove(`${styles.errorMessageShown}`)
+
     if (mode === "edit") {
       const newEditedHabit: Habit = {
         id: habitOnEdition.id,
@@ -87,25 +107,27 @@ export default function AddHabit({
       </div>
       <form className={styles["card__form"]} onSubmit={handleSubmit}>
         <div className={styles["card__input"]}>
-          <input
-            className={styles["input__field"]}
+          <input className={styles["input__field"]}
             type="text"
             placeholder="Enter habit name"
+            ref={refs.name}
             value={habitName}
             onChange={(e) => setHabitName(e.target.value)}
           />
+          <span className={styles.errorMessage}>Enter a name  </span>
         </div>
         <div className={styles["card__color"]}>
           <span className={styles["color__label"]}>Choose a color</span>
-          <div className={styles["color__list"]}>
+          <div className={styles["color__list"]}
+            ref={refs.color}
+          >
             {colors.map((color) => (
               <span
                 key={color.id}
-                className={`${styles["color__item"]} ${
-                  selectedColorId === color.id
-                    ? styles["color__item--selected"]
-                    : ""
-                }`}
+                className={`${styles["color__item"]} ${selectedColorId === color.id
+                  ? styles["color__item--selected"]
+                  : ""
+                  }`}
                 style={
                   {
                     "--background-color": color.hex,
@@ -116,6 +138,7 @@ export default function AddHabit({
               />
             ))}
           </div>
+          <span className={styles.errorMessage}>Select a color</span>
         </div>
         <div className={styles["card__action"]}>
           <button className={styles["button"]} type="submit">
